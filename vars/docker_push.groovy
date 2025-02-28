ff7fd6c5676b3e2bee6d01b4949c2d8ec9588c0b
sidhanshumahajan <47296472+sidhanshumahajan@users.noreply.github.com>
2025-02-28 14:19:44 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,7 +1,7 @@
-def call(String dockerhubuserName, String ProjectName, String ImageTag) {
+def call(String dockerhubuser, String ProjectName, String ImageTag) {
       withCredentials([usernamePassword(credentialsId: 'dockerHubCred', passwordVariable: 'dockerHubPwd', usernameVariable: 'dockerHubUserName')]) {
       sh "docker login -u ${env.dockerHubUserName}  -p ${env.dockerHubPwd}"
       // sh "docker image tag my-spring-app:${formattedDate} ${env.dockerHubUserName}/my-spring-app:${formattedDate}"
     }
-      sh "docker push ${dockerhubuserName}/${ProjectName}:${ImageTag}"
+      sh "docker push ${dockerhubuser}/${ProjectName}:${ImageTag}"
 }
