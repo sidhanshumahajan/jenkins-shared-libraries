98519dd4d8f0e21b24da230ac89c2ef4d3db2ef1
sidhanshumahajan <47296472+sidhanshumahajan@users.noreply.github.com>
2025-02-28 15:10:43 +0530

    Update owasp_dependency.groovy

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,3 +1,4 @@
+// the name use in odcInstallation is define in Tool
 def call(){
   dependencyCheck additionalArguments: '--scan ./', odcInstallation: 'OWASP'
   dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
