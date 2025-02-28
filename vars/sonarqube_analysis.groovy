258e5dd79c405c890972d14425e1f2e3e5ad047d
sidhanshumahajan <47296472+sidhanshumahajan@users.noreply.github.com>
2025-02-28 20:45:18 +0530

    Update sonarqube_analysis.groovy

--- vars/sonarqube_analysis.groovy
+++ vars/sonarqube_analysis.groovy
@@ -2,8 +2,7 @@ def call(String SonarQubeAPI, String ProjectName, String ProjectKey){
   // withSonarQubeEnv("${SonarQubeAPI}"){
   //   sh "$SONAR_HOME/bin/sonar-scanner -Dsonar.projectName=${ProjectName} -Dsonar.projectKey=${ProjectKey} -X"
   // }
-  def mvn = tool 'Default Maven';
     withSonarQubeEnv() {
-      sh "${mvn}/bin/mvn clean verify sonar:sonar -Dsonar.projectKey=${ProjectKey}"
+      sh "mvn clean verify sonar:sonar -Dsonar.projectKey=${ProjectKey}"
     }
 }
